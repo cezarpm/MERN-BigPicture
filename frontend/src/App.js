b9268421ef46713c907d587e47b72701fd56f7ac
cezarpm <cezar_pm@hotmail.com>
2019-11-24 22:50:18 -0300

    frontend started

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>FrontEnd Started</p>
+    </div>
+  );
+}
+
+export default App;
